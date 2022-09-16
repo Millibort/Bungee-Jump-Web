d820c7f175b90e2a5d3dd6be0b700be4d5e83925
Millibort <91936081+Millibort@users.noreply.github.com>
2022-09-15 21:41:42 -0700

    Add files via upload

---
+++ Gametest1/Rays!.js
@@ -0,0 +1,84 @@
+
+var screensetup = function() {
+    //width needs 256
+    //height needs 176
+    var widthlower = Math.floor(swidth/256);
+    var heightlower = Math.floor(sheight/176);
+
+    if(widthlower > heightlower) {mult = heightlower;}
+    else if(widthlower < heightlower) {mult = widthlower;}
+    else {mult = heightlower}
+
+    var widthremain = swidth-(256*mult)
+    var heightremain = sheight-(176*mult)
+
+    left = Math.floor(widthremain/2);
+    zero = Math.floor(heightremain/2);
+
+/*
+    console.log(widthlower);
+    console.log(widthremain);
+    console.log(heightlower);
+    console.log(heightremain);
+
+    console.log("----------------------------------")
+
+    console.log(zero)
+    console.log(left)
+    console.log(mult)
+
+    console.log(swidth);
+    console.log(sheight);
+*/
+    canvas = document.createElement("canvas");
+    canvas.width = swidth;
+    canvas.height = sheight;
+    ctx = canvas.getContext("2d");
+    document.body.insertBefore(canvas, document.body.childNodes[0]);
+    setInterval(Run, 20);
+}
+
+var loaddata = function() {
+    var data = jQuery.getJSON("")
+}
+
+var Run = function() {
+    ctx.fillStyle = "#000000";
+    ctx.beginPath();
+    ctx.rect(0, 0, swidth, sheight);
+    ctx.fill();
+
+    ctx.fillStyle = "#FFFFFF";
+    ctx.beginPath();
+    ctx.rect(left, zero, 256*mult, 176*mult);
+    ctx.fill();
+
+    var i2 = 0;
+    while(i2<11) {
+        var i = 0;
+        while(i<16) {
+            ctx.fillStyle = "#111111";
+            ctx.beginPath();
+            ctx.rect(left + (i*16*mult), zero + (i2*16*mult), 16*mult, 16*mult);
+            ctx.fill(); 
+            i++;
+        }
+        i2++
+    }
+}
+
+//canvas.style.cursor = "none";
+var key = false;
+window.addEventListener('keydown', function (e) {
+    key = e.key;
+})
+  window.addEventListener('keyup', function (e) {
+    key = false;
+})
+
+var mult;
+var zero;
+var left;
+const swidth = window. innerWidth -20;
+const sheight = window. innerHeight -20;
+screensetup();
